cd16aa5e0cc0189f434a2d323689cc3ffa3cdfed
Vladislav Bulantsev <vladislavbv96@gmail.com>
2019-09-19 10:28:56 +0200

    animations started

---
+++ animations.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="animations" tilewidth="1" tileheight="1" tilecount="173010" columns="438">
+ <image source="Possible tilesets and characters/sprites.png" width="438" height="395"/>
+</tileset>
