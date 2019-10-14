f0122c81271ec948bcdfaf078b6c579db226ca8c
Vladislav Bulantsev <vladislavbv96@gmail.com>
2019-10-14 11:59:34 +0200

    spritesheet

--- GDV-Platformer/Game/maps/Tileset.tsx
+++ GDV-Platformer/Game/maps/Tileset.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.2.3" name="Tileset" tilewidth="128" tileheight="128" tilecount="20" columns="5">
- <image source="../../../Possible Tilesets and Characters/Graveyard map/png/Tileset.png" width="640" height="512"/>
+ <image source="Tileset.png" width="640" height="512"/>
 </tileset>
